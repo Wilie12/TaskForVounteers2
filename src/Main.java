b60eb34d3f866c3d67c95cb21fc3259133bbc441
Wilie12 <albertw536@gmail.com>
2024-12-06 18:38:32 +0100

    Fix task4 number range

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(Task1.writeStringForgivenInt(5));
         System.out.println(Arrays.toString(Task3.getIntArrayForGivenInt(3)));
-        System.out.println(Task4.getHighestNumberInFamily(1374));
+        System.out.println(Task4.getHighestNumberInFamily(123));
     }
 }
--- src/Task4.java
+++ src/Task4.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 public class Task4 {
 
     public static int getHighestNumberInFamily(int number) {
+        if (number < 0) return 0;
+        if (number > 10000) return 0;
+
         StringBuilder returnedNumberAsString = new StringBuilder();
         char[] charactersInNumber = String.valueOf(number).toCharArray();
 
